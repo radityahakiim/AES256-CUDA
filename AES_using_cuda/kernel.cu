835bdaa8502459fd9d1321ed79b4da7b42895d76
Raditya H <radityahd39@gmail.com>
2024-11-15 22:32:23 +0700

    for test case

--- AES_using_cuda/kernel.cu
+++ AES_using_cuda/kernel.cu
@@ -4,56 +4,57 @@
 #include <iostream>
 #include <vector>
 
+
 int main() {
 	std::string textKey = "passwordrahasia1234567890";
-	std::string plaintext = "hello";
+	uint8_t plaintext[] = "This is a test message for AES-256 ECB encryption! I love beer! I love Wine!! Thank you based god!!";
 	uint8_t originalKey[AES_KEY_SIZE];
+
+	size_t plaintextLength = strlen((char*)plaintext);
+	size_t paddingSize = AES_BLOCK_SIZE - (plaintextLength % AES_BLOCK_SIZE);
+	size_t padded_len = plaintextLength + paddingSize;
+
+	std::vector<uint8_t> ciphertext(padded_len, 0);
+	std::vector<uint8_t> paddedPlaintext(padded_len, 0);
+	paddedPlaintext.resize(padded_len, paddingSize);
+
+	// Copy plaintext and apply PKCS#7 padding
+	memcpy(paddedPlaintext.data(), plaintext, plaintextLength);
+	std::fill(paddedPlaintext.begin() + plaintextLength, paddedPlaintext.end(), paddingSize);
+
+	// convert string to aes key
 	convertStringToAESKey(textKey, originalKey);
+	// expand the key for encryption purposes
+	const size_t roundKeysSize = AES_EXPANDED_KEY_SIZE;
+	uint8_t expandedKey[roundKeysSize];
+	keyExpansion(expandedKey, originalKey);
 
-	uint8_t h_expandedKey[AES_EXPANDED_KEY_SIZE];
-	uint8_t h_originalKey[AES_KEY_SIZE];
-	uint8_t *d_originalKey, *d_expandedKey, *d_state;
-
-	// allocate mem on device
-	cudaMalloc((void**)&d_originalKey, AES_KEY_SIZE * sizeof(uint8_t));
-	cudaMalloc((void**)&d_expandedKey, AES_EXPANDED_KEY_SIZE * sizeof(uint8_t));
-	cudaMalloc((void**)&d_state, Nb * 4);
-
-	// Copy original key to device
-	cudaMemcpy(d_originalKey, originalKey, AES_KEY_SIZE * sizeof(uint8_t), cudaMemcpyHostToDevice);
-
-	// Launch kernel
-	int threadsPerBlock = 8;
-	keyExpansionKernel << < 1, threadsPerBlock >> > (d_expandedKey, d_originalKey);
-	cudaDeviceSynchronize();
-
-	uint8_t roundKey[16]; // Start of applying the AddRoundKey for round 0, assuming 16 bytes for one round key first
-	// Copy expanded key back to host
-	cudaMemcpy(h_originalKey, d_originalKey, AES_KEY_SIZE * sizeof(uint8_t), cudaMemcpyDeviceToHost);
-	cudaMemcpy(h_expandedKey, d_expandedKey, AES_EXPANDED_KEY_SIZE * sizeof(uint8_t), cudaMemcpyDeviceToHost);
-	AddRoundKey<<<1, AES_BLOCK_SIZE>>>(d_state, roundKey);
-	cudaDeviceSynchronize();
-
-	// Memory cleanup
-	cudaFree(d_originalKey);
-	cudaFree(d_expandedKey);
-	cudaFree(d_state);
-
-	std::vector<uint8_t> plaintextBlocks = preparePlaintext(plaintext);
 	std::cout << "Padded Plaintext in hex :" << std::endl;
-	printHex(plaintextBlocks);
+	for (size_t i = 0; i < padded_len; ++i) {
+		printf("%02x ", paddedPlaintext[i]);
+		if ((i + 1) % 16 == 0) std::cout << std::endl;
+	}
 
 	std::cout << "Original Key: " << std::endl;
 	for (int i = 0; i < AES_KEY_SIZE; i++) {
-		printf("%02x ", h_originalKey[i]);
+		printf("%02x ", originalKey[i]);
 		if ((i + 1) % 16 == 0) std::cout << std::endl;
 	}
 
 	//Print the expanded key
 	std::cout << "Expanded Key: " << std::endl;
 	for (int i = 0; i < AES_EXPANDED_KEY_SIZE; i++) {
-		printf("%02x ", h_expandedKey[i]);
-	 	if ((i + 1) % 16 == 0) std::cout << std::endl;
+		printf("%02x ", expandedKey[i]);
+		if ((i + 1) % 16 == 0) std::cout << std::endl;
 	}
+
+	h_AESEncryptECB(paddedPlaintext.data(), originalKey, ciphertext.data(), padded_len);
+	std::cout << "Ciphertext: " << std::endl;
+	for (size_t i = 0; i < padded_len; ++i) {
+		printf("%02x ", ciphertext[i]);
+		if ((i + 1) % 16 == 0) std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
