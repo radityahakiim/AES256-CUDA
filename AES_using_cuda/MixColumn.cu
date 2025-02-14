5b7b77e4c90d0588485a66130c43151ec2ff608d
Raditya H <radityahd39@gmail.com>
2025-02-14 20:20:30 +0700

    use bitwise shifts on MixColumn

--- AES_using_cuda/MixColumn.cu
+++ AES_using_cuda/MixColumn.cu
@@ -3,7 +3,7 @@
 #include "aes_header.cuh"
 
 static __device__ __forceinline__ uint8_t xtime(uint8_t x) {
-    return ((x << 1) ^ (((x >> 7) & 1) * 0x1b));
+    return ((x << 1) ^ (-(x >> 7) & 1) & 0x1b);
 }
 
 static __device__ __forceinline__ uint8_t Multiply(uint8_t x, uint8_t y) {
@@ -12,11 +12,11 @@ static __device__ __forceinline__ uint8_t Multiply(uint8_t x, uint8_t y) {
     uint8_t xt3 = xtime(xt2);
     uint8_t xt4 = xtime(xt3);
 
-    return (((y & 1) * x) ^
-        ((y >> 1 & 1) * xt1) ^
-        ((y >> 2 & 1) * xt2) ^
-        ((y >> 3 & 1) * xt3) ^
-        ((y >> 4 & 1) * xt4));
+    return ((-(y & 1) & x) ^
+        (-(y >> 1 & 1) & xt1) ^
+        (-(y >> 2 & 1) & xt2) ^
+        (-(y >> 3 & 1) & xt3) ^
+        (-(y >> 4 & 1) & xt4));
 }
 
 __device__ void MixColumns(state_t* state) {
@@ -29,28 +29,32 @@ __device__ void MixColumns(state_t* state) {
 
     // Compute the MixColumns transformation using xtime and XORs
     uint8_t t = a0 ^ a1 ^ a2 ^ a3;
-    uint8_t tmp0 = xtime(a0 ^ a1) ^ t ^ a0;
-    uint8_t tmp1 = xtime(a1 ^ a2) ^ t ^ a1;
-    uint8_t tmp2 = xtime(a2 ^ a3) ^ t ^ a2;
-    uint8_t tmp3 = xtime(a3 ^ a0) ^ t ^ a3;
+    uint8_t u = xtime(a0 ^ a1);
+    uint8_t v = xtime(a1 ^ a2);
+    uint8_t w = xtime(a2 ^ a3);
+    uint8_t x = xtime(a3 ^ a0);
 
-    // Synchronize the warp and shuffle results
-    (*state)[0][lane] = tmp0;
-    (*state)[1][lane] = tmp1;
-    (*state)[2][lane] = tmp2;
-    (*state)[3][lane] = tmp3;
+    (*state)[0][lane] = u ^ t ^ a0;
+    (*state)[1][lane] = v ^ t ^ a1;
+    (*state)[2][lane] = w ^ t ^ a2;
+    (*state)[3][lane] = x ^ t ^ a3;
 }
 
 __device__ void InvMixColumns(state_t* state) {
     int lane = threadIdx.x & 3;
-    uint8_t a, b, c, d;
-    a = (*state)[0][lane];
-    b = (*state)[1][lane];
-    c = (*state)[2][lane];
-    d = (*state)[3][lane];
 
-    (*state)[0][lane] = Multiply(a, 0x0e) ^ Multiply(b, 0x0b) ^ Multiply(c, 0x0d) ^ Multiply(d, 0x09);
-    (*state)[1][lane] = Multiply(a, 0x09) ^ Multiply(b, 0x0e) ^ Multiply(c, 0x0b) ^ Multiply(d, 0x0d);
-    (*state)[2][lane] = Multiply(a, 0x0d) ^ Multiply(b, 0x09) ^ Multiply(c, 0x0e) ^ Multiply(d, 0x0b);
-    (*state)[3][lane] = Multiply(a, 0x0b) ^ Multiply(b, 0x0d) ^ Multiply(c, 0x09) ^ Multiply(d, 0x0e);
+    uint8_t a = (*state)[0][lane];
+    uint8_t b = (*state)[1][lane];
+    uint8_t c = (*state)[2][lane];
+    uint8_t d = (*state)[3][lane];
+
+    uint8_t s = Multiply(a, 0x0e) ^ Multiply(b, 0x0b) ^ Multiply(c, 0x0d) ^ Multiply(d, 0x09);
+    uint8_t t = Multiply(a, 0x09) ^ Multiply(b, 0x0e) ^ Multiply(c, 0x0b) ^ Multiply(d, 0x0d);
+    uint8_t u = Multiply(a, 0x0d) ^ Multiply(b, 0x09) ^ Multiply(c, 0x0e) ^ Multiply(d, 0x0b);
+    uint8_t v = Multiply(a, 0x0b) ^ Multiply(b, 0x0d) ^ Multiply(c, 0x09) ^ Multiply(d, 0x0e);
+
+    (*state)[0][lane] = s;
+    (*state)[1][lane] = t;
+    (*state)[2][lane] = u;
+    (*state)[3][lane] = v;
 }
