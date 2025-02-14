88d6aa9dff3370171154c275de0611fd005988f7
Raditya H <radityahd39@gmail.com>
2025-02-14 20:17:32 +0700

    use uint32_t instead of uint8_t for roundKey

--- AES_using_cuda/AddRoundKey.cu
+++ AES_using_cuda/AddRoundKey.cu
@@ -2,17 +2,15 @@
 #include "device_launch_parameters.h"
 #include "aes_header.cuh"
 
-__device__ void AddRoundKey(state_t* state, uint8_t round, const uint8_t* roundKey) {
+__device__ void AddRoundKey(state_t* state, uint8_t round, const uint32_t* roundKey) {
+	size_t offset = (size_t)round * 4;
+	const uint32_t* roundKeyStart = roundKey + offset;
+
+	// Reinterpret state as uint32_t for efficient word-wise operations
+	uint32_t* state_as_words = reinterpret_cast<uint32_t*>(*state);
+
 #pragma unroll
 	for (uint8_t i = 0; i < 4; i++) {
-#pragma unroll
-		for (uint8_t j = 0; j < 4; j++) {
-			uint32_t index = (round * Nb * 4) + (i * Nb) + j;
-			if (index >= AES_EXPANDED_KEY_SIZE) {
-				printf("Out of bounds access in AddRoundKey at index %u\n", index);
-				return;
-			}
-			(*state)[i][j] ^= roundKey[index];
-		}
+		state_as_words[i] ^= roundKeyStart[i];
 	}
 }
