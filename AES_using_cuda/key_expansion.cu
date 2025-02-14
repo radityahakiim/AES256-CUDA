dc4cb265ce45ad9ceb65aed856caaed50965b62e
Raditya H <radityahd39@gmail.com>
2025-02-14 20:19:53 +0700

    use bitwise instead

--- AES_using_cuda/key_expansion.cu
+++ AES_using_cuda/key_expansion.cu
@@ -11,52 +11,46 @@
 extern uint8_t Rcon[8] = { 0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40 };
 
 // Function for expanding the AES key
-void keyExpansion(uint8_t* expandedKey, const uint8_t* originalKey) {
-    uint32_t temp[4];
-    unsigned i, j, k;
+void keyExpansion(uint32_t* expandedKey, const uint8_t* originalKey) {
+    uint32_t temp;
+    unsigned i;
 
     // Copy original key to expanded key
     for (i = 0; i < Nk; i++) {
-        expandedKey[(i * 4) + 0] = originalKey[(i * 4) + 0];
-        expandedKey[(i * 4) + 1] = originalKey[(i * 4) + 1];
-        expandedKey[(i * 4) + 2] = originalKey[(i * 4) + 2];
-        expandedKey[(i * 4) + 3] = originalKey[(i * 4) + 3];
+        expandedKey[i] = (originalKey[(i * 4) + 0] << 24) |
+            (originalKey[(i * 4) + 1] << 16) |
+            (originalKey[(i * 4) + 2] << 8) |
+            originalKey[(i * 4) + 3];
     }
 
     // Key expansion loop
     for (i = Nk; i < Nb * (Nr + 1); i++) {
-        k = (i - 1) * 4;
-        temp[0] = expandedKey[k + 0];
-        temp[1] = expandedKey[k + 1];
-        temp[2] = expandedKey[k + 2];
-        temp[3] = expandedKey[k + 3];
+        temp = expandedKey[i - 1];
 
         if (i % Nk == 0) {
-            const uint8_t tempb = temp[0];
-            temp[0] = temp[1];
-            temp[1] = temp[2];
-            temp[2] = temp[3];
-            temp[3] = tempb;
+            // Rotate
+            temp = (temp << 8) | ((temp >> 24) & 0xFF);
 
-            temp[0] = getSBoxValue(temp[0]);
-            temp[1] = getSBoxValue(temp[1]);
-            temp[2] = getSBoxValue(temp[2]);
-            temp[3] = getSBoxValue(temp[3]);
+            // Substitute with S-box values each byte
+            temp = (getSBoxValue((temp >> 24) & 0xFF) << 24) |
+                (getSBoxValue((temp >> 16) & 0xFF) << 16) |
+                (getSBoxValue((temp >> 8) & 0xFF) << 8) |
+                getSBoxValue(temp & 0xFF);
 
-            temp[0] = temp[0] ^ Rcon[i / Nk];
+            // Rcon XOR
+            temp = temp ^ (Rcon[i / Nk] << 24);
         }
-        if (i % Nk == 4) {
-            temp[0] = getSBoxValue(temp[0]);
-            temp[1] = getSBoxValue(temp[1]);
-            temp[2] = getSBoxValue(temp[2]);
-            temp[3] = getSBoxValue(temp[3]);
+        else if (Nk > 6 && i % Nk == 4)
+        {
+            // For AES-256, apply S-box to each byte
+            temp = (getSBoxValue((temp >> 24) & 0xFF) << 24) |
+                (getSBoxValue((temp >> 16) & 0xFF) << 16) |
+                (getSBoxValue((temp >> 8) & 0xFF) << 8) |
+                getSBoxValue(temp & 0xFF);
         }
 
-        j = i * 4; k = (i - Nk) * 4;
-        expandedKey[j + 0] = expandedKey[k + 0] ^ temp[0];
-        expandedKey[j + 1] = expandedKey[k + 1] ^ temp[1];
-        expandedKey[j + 2] = expandedKey[k + 2] ^ temp[2];
-        expandedKey[j + 3] = expandedKey[k + 3] ^ temp[3];
+        // XOR with the word Nk positions back
+        expandedKey[i] = expandedKey[i - Nk] ^ temp;
     }
 }
 
